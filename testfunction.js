bb9f7be323660179012b5538095519066e2986ce
AlexKuzmenko <AlexKuzmenko2000@gmail.com>
2020-12-15 12:39:54 +0200

    TestSystemAPI.V01

--- testfunction.js
+++ testfunction.js
@@ -1,13 +1,16 @@
 var assert = require('chai').assert;
-function  conversion(a, b) {
-    var c = (a / b).toFixed(2);
-    return c;
+function sum_in_interval(a, b) {
+var sum = 0;
+for (var i = a; i <= b; i++) {
+sum += i;
+}
+return sum;
 }
 describe('myFunction()', function() {
-var tests = [{args: [1, 27], expected: 0.04},{args: [27, 27], expected: 1},{args: [50, 27], expected: 1.85},{args: [5000, 30], expected: 166.66},];
+var tests = [{args: [5, 6], expected: 11},{args: [5, 8], expected: 26},{args: [1, 100], expected: 5050},{args: [-1, 1], expected: 0},];
 tests.forEach(function(test) {
         it('correctly adds ' + test.args.length + ' args', function() {
-            var res = conversion.apply(null, test.args);
+            var res = sum_in_interval.apply(null, test.args);
             assert.equal(res, test.expected);
         });
     });});
