87c7fe05455b6dded9d672515b3b06f4d8a10e78
AlexKuzmenko <AlexKuzmenko2000@gmail.com>
2020-12-17 11:56:38 +0200

    asdf

--- src/app.js
+++ src/app.js
@@ -1,76 +1,100 @@
 var Mocha = require('mocha');
 const express = require('express')
-const bodyParser = require('body-parser');
+const mysql = require("mysql")
+const schedule = require('node-schedule');
 const fs = require("fs")
-const app = express()
-const port = 3000
-app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.post('/', function (req, res) {
-    var solution = req.body.solution;
-    var functionName = req.body.function;
-    var testsJSON = req.body.tests;
-    var testsArray = JSON.parse(testsJSON);
-    var tests = [];
-    for (var i = 0; i < testsArray.args.length; i++) {
-        if (testsArray.args[i].length > 0) {
-            tests.push({
-                args: testsArray.args[i].split(", "),
-                expected: testsArray.result[i]
-            })
+
+// const bodyParser = require('body-parser');
+
+// const app = express()
+// const port = 3000
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+
+var j = schedule.scheduleJob('*/2 * * * * *', function(){
+    var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        database : 'test_system'
+    });
+
+    connection.query('SELECT * FROM solution LEFT JOIN task ON solution.task_id=task.task_id  WHERE solution.status=0', function (error, results, fields) {
+        if (error) throw error;
+        if (results) {
+            for (var j = 0; j < results.length; j++) {
+                var solution_id = results[j].solution_id;
+                var solution = results[j].solution;
+                var functionName = results[j].function_name;
+                var testsJSON = results[j].tests;
+                var testsArray = JSON.parse(testsJSON);
+                var tests = [];
+                for (var i = 0; i < testsArray.args.length; i++) {
+                    if (testsArray.args[i].length > 0) {
+                        tests.push({
+                            args: testsArray.args[i].split(", "),
+                            expected: testsArray.result[i]
+                        })
+                    }
+                }
+                console.log(tests);
+                var testOut = "["
+                for (var i = 0; i < tests.length; i++) {
+                    testOut += `{args: [${tests[i].args.join(", ")}], expected: ${tests[i].expected}},`;
+                }
+                testOut += "]";
+
+                console.log(tests);
+                let file = "testfunction.js";
+
+                fs.writeFileSync(file, "var assert = require('chai').assert;\n");
+                fs.appendFileSync(file, solution + "\n");
+                fs.appendFileSync(file, "describe('myFunction()', function() {\n");
+                fs.appendFileSync(file, "var tests = " + testOut + ";\n");
+                fs.appendFileSync(file, "tests.forEach(function(test) {\n" +
+                    "        it('correctly adds ' + test.args.length + ' args', function() {\n" +
+                    "            var res = " + functionName + ".apply(null, test.args);\n" +
+                    "            assert.equal(res, test.expected);\n" +
+                    "        });\n" +
+                    "    });")
+
+                fs.appendFileSync(file, "});")
+
+                var mocha = new Mocha({});
+                var results = [];
+
+                mocha.addFile(file)
+                var passed = 0;
+                var failed = 0;
+                mocha.run()
+                    .on('pass', function (test) {
+                        passed++;
+                        results.push({message: "Test passed"})
+                    })
+                    .on('fail', function (test, err) {
+                        failed++;
+                        results.push({message: "Test failed", error: err})
+                    })
+                    .on('end', function () {
+                        result = passed / (passed + failed)
+                        //res.send({results, result});
+                        //console.log({results, result});
+                        var connection = mysql.createConnection({
+                            host     : 'localhost',
+                            user     : 'root',
+                            password : '',
+                            database : 'test_system'
+                        });
+                        connection.query(`UPDATE solution SET test_result='${JSON.stringify({results, result})}', result=${result}, status=1 WHERE solution_id=${solution_id}`, function (error, results1, fields) {
+                            if (error) throw error;
+                            console.log(solution_id);
+                            connection.end();
+                        });
+                    });
+            }
         }
-    }
-    console.log(tests);
-    var testOut = "["
-    for(var i = 0; i < tests.length; i++) {
-        testOut += `{args: [${tests[i].args.join(", ")}], expected: ${tests[i].expected}},`;
-    }
-    testOut += "]";
-
-    console.log(tests);
-    let file = "testfunction.js";
-
-    fs.writeFileSync(file, "var assert = require('chai').assert;\n");
-    fs.appendFileSync(file, solution + "\n");
-    fs.appendFileSync(file, "describe('myFunction()', function() {\n");
-    fs.appendFileSync(file, "var tests = " + testOut + ";\n");
-    fs.appendFileSync(file, "tests.forEach(function(test) {\n" +
-        "        it('correctly adds ' + test.args.length + ' args', function() {\n" +
-        "            var res = " + functionName + ".apply(null, test.args);\n" +
-        "            assert.equal(res, test.expected);\n" +
-        "        });\n" +
-        "    });")
-
-    fs.appendFileSync(file, "});")
-
-    var mocha = new Mocha({});
-    var results = [];
-
-    mocha.addFile(file)
-    var passed = 0;
-    var failed = 0;
-    mocha.run()
-        .on('test', function(test) {
-            //console.log('Test started: '+test.title);
-        })
-        .on('test end', function(test) {
-            //console.log('Test done: '+test.title);
-        })
-        .on('pass', function(test) {
-            passed++;
-            results.push({message: "Test passed"})
-        })
-        .on('fail', function(test, err) {
-            failed++;
-            results.push({message: "Test failed", error: err})
-        })
-        .on('end', function() {
-            result = passed / (passed + failed)
-            res.send({results, result});
-        });
-})
-
-app.listen(port, () => {
-    console.log(`Listening on ${port}`)
-})
+    })
+    connection.end();
+});
--- testfunction.js
+++ testfunction.js
@@ -1,16 +1,10 @@
 var assert = require('chai').assert;
-function sum_in_interval(a, b) {
-var sum = 0;
-for (var i = a; i <= b; i++) {
-sum += i;
-}
-return sum;
-}
+function hypotenuse(a, b) {return Math.sqrt(a * a + b * b); } 
 describe('myFunction()', function() {
-var tests = [{args: [5, 6], expected: 11},{args: [5, 8], expected: 26},{args: [1, 100], expected: 5050},{args: [-1, 1], expected: 0},];
+var tests = [{args: [3, 4], expected: 5},{args: [6, 8], expected: 10},{args: [12, 16], expected: 20},{args: [24, 32], expected: 40},];
 tests.forEach(function(test) {
         it('correctly adds ' + test.args.length + ' args', function() {
-            var res = sum_in_interval.apply(null, test.args);
+            var res = hypotenuse.apply(null, test.args);
             assert.equal(res, test.expected);
         });
     });});
